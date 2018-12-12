18044a07404acb85812617a4e75e8d7ddd908bdc
chagay levi <chagay88@gmail.com>
2018-12-12 19:03:23 +0200

    first commit

---
+++ app/src/main/java/com/chagay/gettexid/controller/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.chagay.gettexid.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.chagay.gettexid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_login );
+    }
+}
